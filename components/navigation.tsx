f6236741e2c0a3db05716bdaa5ade025b8337022
kouya17 <17383696+kouya17@users.noreply.github.com>
2022-08-19 23:40:18 +0900

    update

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { FunctionComponent } from "react";
 import { MenuItem } from "../lib/models";
 
@@ -9,15 +10,19 @@ interface NavigationProps {
 const Navigation: FunctionComponent<NavigationProps> = ({ title, menu }) => (
   <nav className="bg-white border-b-2 border-gray-300 px-2 py-2.5 dark:bg-gray-900 sticky top-0 z-50">
     <div className="container px-5 flex flex-wrap justify-between items-center mx-auto">
-      <a href="/" className="flex items-center">
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">TOP</span>
-      </a>
+      <Link href="/">
+        <a className="flex items-center">
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">TOP</span>
+        </a>
+      </Link>
       <div className="block w-auto">
         <ul className="flex border-gray-100 flex-row space-x-8 mt-0 text-sm font-medium border-0 dark:bg-gray-900">
-          {menu.map((e) => {
+          {menu.map((e, i) => {
             return (
-              <li>
-                <a href={e.path} className="bg-blue-700 rounded bg-transparent text-blue-700 p-0 dark:text-white">{e.name}</a>
+              <li key={i}>
+                <Link href={e.path}>
+                  <a className="bg-blue-700 rounded bg-transparent text-blue-700 p-0 dark:text-white">{e.name}</a>
+                </Link>
               </li>
             )
           })}
